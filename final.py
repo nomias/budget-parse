03ac552e713275249a967cb7249eed04863c3db7
Saimon Shrestha <sai.shrestha14@gmail.com>
2023-04-23 18:48:45 -0400

    budget,revenue,profit is made

--- final.py
+++ final.py
@@ -1,34 +1,13 @@
 import csv
 import pandas as pd
 import numpy as np
-#from sklearn.linear_model import LinearRegression,Ridge,Lasso
 
-bud=pd.read_csv('budgetse.csv')
-imdb=pd.read_csv('imdb_top_1000 copy.csv')
+imdb=pd.read_csv("imdb_top_1000.csv")
+budgets=pd.read_csv("budgets.csv")
 
-#print(imdb)
-budList=bud['Title']
-imdbList=imdb['Series_Title']
-bud.info()
-imdb.info()
-bud.drop_duplicates()
-bud.to_csv("test.csv",index=False)
-#imdb.drop(imdb.tail(500).index,inplace=True)
-#budgets=pd.DataFrame({'Budget':np.array(bud['Budget'].values)})
-#print(budgets)
-#imdbList.update(budgets.any())
-#imdb['Budget']=bud['Budget'].values
-#i=1
-#for n in imdbList:
-    #for m in budList:
-        #print(i)
-        #i+=1
-        #if (n==m):
-            #print(n,m)
-            #row=imdb[imdb['Series_Title'] == m].index
-            #print(row)
-            #budget=bud.loc[budList==n,'Budget'].values
-            #imdb.at[row,'Budget']=budget
-
-
-#lr=LinearRegression
+imdbTitles=imdb['Series_Title']
+budgetTitles=budgets['Title']
+for imdbTit in imdbTitles:
+    for budgetTit in budgetTitles:
+        if imdbTit==budgetTit:
+            imdb.loc[imdbTitles==budgetTit,"Budget"].values=budgets.loc[budgetTitles==budgetTit,"Budget"].values
--- parse.py
+++ parse.py
@@ -10,24 +10,25 @@ movieTitles=movies['original_title']
 
 for imdbTit in imdbTitles:
     #print(imdbTit)
-    imdbYearArr=imdb.loc[imdbTitles==imdbTit,'Released_Year'].values
-    imdbYear=imdbYearArr[0]
+    #imdbYearArr=imdb.loc[imdbTitles==imdbTit,'Released_Year'].values
+    #imdbYear=imdbYearArr[0]
     #print(imdbTit,imdbYear)
     for movieTit in movieTitles:
         #numpy.ndarray
-        movieYear=0
-        movieYearArr=movies.loc[movieTitles==imdbTit,'release_date'].values
-        if isinstance(movieYearArr,(np.ndarray)):
-            movieYear=int(movieYearArr[0].split('-',1)[0])
-        else:
-            movieYear=movieYearArr
-        print(movieTit,imdbTit,movieYear)
+        #movieYear=0
+        #movieYearArr=movies.loc[movieTitles==imdbTit,'release_date'].values
+        #if isinstance(movieYearArr,(np.ndarray)):
+        #    movieYear=int(movieYearArr[0].split('-',1)[0])
+        #else:
+        #    movieYear=movieYearArr
+        #print(movieTit,imdbTit,movieYear)
         #movieYear=int(movieYearArr[0].split('-',1)[0])
         #print(movieTit,movieYear)
-        if (imdbTit==movieTit) & (imdbYear==movieYear):
+        if (imdbTit==movieTit):
             print("success")
             budget=movies.loc[movieTitles==movieTit,'budget'].values
-            with open('budgets.csv','a',newline='') as file:
+            revenue=movies.loc[movieTitles==movieTit,'revenue'].values
+            profit=revenue-int(budget.any())
+            with open('budgets2.csv','a',newline='') as file:
                 writer = csv.writer(file)
-                writer.writerow([movieTit,budget])
-        break
+                writer.writerow([movieTit,budget,revenue,profit])
